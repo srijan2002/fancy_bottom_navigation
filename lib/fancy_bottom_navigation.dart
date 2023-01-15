db189fb6d3422de8cb4a4b655f04dcc4a8d6e568
Srijan Majumdar <77445478+srijan2002@users.noreply.github.com>
2023-01-15 18:45:54 +0530

    tfhgj

--- lib/fancy_bottom_navigation.dart
+++ lib/fancy_bottom_navigation.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 
 const double CIRCLE_SIZE = 50;
 const double ARC_HEIGHT = 80;
-const double ARC_WIDTH = 100;
+const double ARC_WIDTH = 160;
 const double CIRCLE_OUTLINE = 10;
 const double SHADOW_ALLOWANCE = 20;
 const double BAR_HEIGHT = 60;
