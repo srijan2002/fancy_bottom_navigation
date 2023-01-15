0a0cdeb412fe418a310d85b86922ae99473b5156
Srijan Majumdar <77445478+srijan2002@users.noreply.github.com>
2023-01-15 18:37:12 +0530

    wert

--- lib/fancy_bottom_navigation.dart
+++ lib/fancy_bottom_navigation.dart
@@ -5,11 +5,11 @@ import 'package:fancy_bottom_navigation_2/paint/half_clipper.dart';
 import 'package:fancy_bottom_navigation_2/paint/half_painter.dart';
 import 'package:flutter/material.dart';
 
-const double CIRCLE_SIZE = 50;
-const double ARC_HEIGHT = 58;
+const double CIRCLE_SIZE = 5;
+const double ARC_HEIGHT = 70;
 const double ARC_WIDTH = 100;
-const double CIRCLE_OUTLINE = 8;
-const double SHADOW_ALLOWANCE = 25;
+const double CIRCLE_OUTLINE = 10;
+const double SHADOW_ALLOWANCE = 20;
 const double BAR_HEIGHT = 60;
 
 class FancyBottomNavigation extends StatefulWidget {
